d8daffe766a73ff2653fdb1f7f06fac1439bcacb
Soso1010 <99210584+Soso1010@users.noreply.github.com>
2022-11-22 16:05:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,58 @@
-function randomNumber{
-array1=[‘pen’,’paper’,’book’,’bottle’]
-randomNo = Math.floor((Math.random() *array1.length)+1)
+function updateCanvas(){
+  background("white");
+  randomNumber = Math.floor((Math.random() * quickDrawDataset.lenght) + 1);
+  console.log(quickDrawDataset[randomNumber]);
+  sketch = quickDrawDataset[randomNumber];
+  document.getElementById('sketchName').innerHTML = 'Esboço a ser desenhado : ' + sketch;
 }
+
+function preload(){
+  classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function setup(){
+  canvas = createCanvas(280, 280);
+  canvas.center();
+  background("white");
+  canvas.mouseReleased(classifyCanvas);
+}
+
+function draw(){
+  strokeWeight(13):
+  stroke(0);
+  
+  if (mouseIsPressed){
+    line(pmouseX, pmouseY, mouseX, mouseY);
+  }
+  
+  checkSketch()
+  if(drawnSketch == sketch)
+  {
+    answerHolder = "set"
+    score++;
+    document.getElementById('score').innerHTML = 'Pontuação: ' + score;
+  }
+  
+  function classifyCanvas(){
+    classifier.classify(canvas, gotResult);
+  }
+  
+  function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    }
+    console.log(results);
+    drawnSketch = results[0].label;
+    document.getElementById('label').innerHTML = 'Seu esboço: ' + drawnSketch.replace("_", " ");
+    
+    
+    document.getElementById('confidence').innerHTML = 'Precisão: ' + Math.round(results[0].confidence * 100) + '%";
+    
+  
+  
+  
+  
+  
+
+
 
